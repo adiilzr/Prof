e4e923270de840c99fada987a82d6e2a505d5308
adilelbagbagui <adiilelbakbak@gmail.com>
2026-02-26 10:44:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-from file1 import Professeur
+from file1 import professeur
+import json  
+with open("professeurs.json", mode="r", encoding="utf-8") as f:    lista=json.load(f)
 
-p1=Professeur("tazi","aziz","fes",5000)
-p2=Professeur("jilali","driss","tanger",4500)
-p3=Professeur("amzil","foad","safrou",7000)
 
-anc1=input(f"saisir l'anciennete de profeseur:{p1.nom} ")
-anc2=input(f"saisir l'anciennete de profeseur:{p2.nom} ")
-anc3=input(f"saisir l'anciennete de profeseur:{p3.nom} ")
+listprofs=[]
+for dicto in lista:
+    listprofs.append(professeur.fromDictr(dicto))
 
-p1.anciennete=int(anc1)
-p2.anciennete=int(anc2)
-p3.anciennete=int(anc3)
-
-print(f"le salaire total de {p1.nom} : {p1.calculerSalaireTotal()} DH")
-print(f"le salaire total de {p2.nom} : {p2.calculerSalaireTotal()} DH")
-print(f"le salaire total de {p3.nom} : {p3.calculerSalaireTotal()} DH")
-
-p1.enregistrer()
-p2.enregistrer()
-p3.enregistrer()
+print("nombre de professeurs:",len(lista))
+print("le professur 89 est:",listprofs[89].nom,listprofs[89].prenom)
