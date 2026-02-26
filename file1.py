484cfc835e7d84166d796fd66c93cdb4a13d40bc
adilelbagbagui <adiilelbakbak@gmail.com>
2026-02-26 10:43:30 +0100

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,39 +1,39 @@
 import json
-class Professeur:
 
-    def __init__(self,n,p,a,s):
-        self.nom=n
-        self.prenom=p
-        self.tel=""
-        self.specialite=""
-        self.salaire=s
-        self.anciennete=0
-        self.etatFAmilial=""
-        self.dateNaissance=""
-        self.adresse=a
-        self.email=""
+class professeur:
+    def __init__(self, N, p, a, s):
+        self.nom = N
+        self.prenom = p
+        self.tel = ""
+        self.specialite = ""
+        self.salaire = s
+        self.anciennete = 0
+        self.etat_familial = ""
+        self.dateNaissance = ""
+        self.adresse = a
+        self.email = ""
 
-    def calculerSalaireTotal(self):
-            return self.salaire+ (self.anciennete//5 * 2000)
-    def toDict(self):
-         dictprof={
-             "nom":self.nom,
-             "prenom":self.prenom,
-             "tel":self.tel,
-             "specialite":self.specialite,
-             "salaire":self.salaire,
-             "anciennete":self.anciennete,
-             "etatFAmilial":self.etatFAmilial,
-             "dateNaissance":self.dateNaissance,
-             "adresse":self.adresse,
-             "email":self.email
-         }
-         return dictprof
-    def enregistrer(self):
-        with open(f"prof{self.nom}_{self.prenom}.json",mode="w") as f:
-            json.dump(self.toDict(),f)
-          
-            
+    def calculsalairetotal(self):
+        return self.salaire + (self.anciennete // 5) * 2000
+
+    def toDictr(self):
+        dictprofesseur = {
+            "nom": self.nom,
+            "prenom": self.prenom,
+            "tel": self.tel,
+            "specialite": self.specialite,
+            "salaire": self.salaire,
+            "anciennete": self.anciennete,
+            "etat_familial": self.etat_familial,
+            "dateNaissance": self.dateNaissance,
+            "adresse": self.adresse,
+            "email": self.email
+        }
+        return dictprofesseur
+
+    @staticmethod
+    def fromDictr(dictprofesseur):
+        N = dictprofesseur.get("nom", "")
          
         
       
